c2a34e243f5099d2875c60f046edf9856face6e5
ydm8964 <ydm8964@users.noreply.github.com>
2026-09-08 15:57:34 +0800

    Fix deployment and shop handlers; refresh service worker cache

--- sw.js
+++ sw.js
@@ -1,21 +1,27 @@
-const CACHE_NAME = 'delta-force-v1';
-const urlsToCache = [
-  '/index.html',
-  '/manifest.json',
-  '/favicon.ico'
-];
+const CACHE_NAME = 'delta-force-v3';
+const APP_SHELL = './';
 
 self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll([
+    './', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './favicon.ico'
+  ]).catch(() => undefined)));
+});
+
+self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
   );
 });
 
 self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return;
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    fetch(event.request).then(response => {
+      const copy = response.clone();
+      caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+      return response;
+    }).catch(() => caches.match(event.request).then(response => response || caches.match(APP_SHELL)))
   );
 });
